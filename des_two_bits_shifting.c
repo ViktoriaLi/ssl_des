385b67596cb26476ca224daafb357fc56e8f2df7
Viktoriia LIKHOTKINA <viktoria.likhotkina@gmail.com>
2018-06-10 17:11:19 +0300

    Norme at s_blocks

--- des_two_bits_shifting.c
+++ des_two_bits_shifting.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   des_two_bits_shifting.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlikhotk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/10 16:57:58 by vlikhotk          #+#    #+#             */
+/*   Updated: 2018/06/10 16:59:36 by vlikhotk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ssl.h"
 
-void set_two_right_bits(unsigned char key_res[], unsigned char key_56[],
+void	set_two_right_bits(unsigned char key_res[], unsigned char key_56[],
 int bit0, int bit1)
 {
 	key_res[1] = ((key_56[0] & 255) << 6) + (key_56[1] >> 2);
@@ -21,23 +33,21 @@ int bit0, int bit1)
 
 void	two_bit_shift_right(unsigned char key_56[], t_args *params)
 {
-	int				i;
-	int				bit0;
-	int				bit1;
+	int						i;
+	int						bit0;
+	int						bit1;
 	static unsigned char	key_res[7];
 
 	i = 0;
 	bit0 = (1 << 4) & key_56[3];
 	bit1 = (1 << 5) & key_56[3];
 	key_res[0] = ((key_56[0] & 255) >> 2);
+	key_res[0] &= ~(1 << 6);
 	if (bit0)
 		key_res[0] |= (1 << 6);
-	else
-		key_res[0] &= ~(1 << 6);
+	key_res[0] &= ~(1 << 7);
 	if (bit1)
 		key_res[0] |= (1 << 7);
-	else
-		key_res[0] &= ~(1 << 7);
 	bit0 = (1 << 0) & key_56[6];
 	bit1 = (1 << 1) & key_56[6];
 	set_two_right_bits(key_res, key_56, bit0, bit1);
@@ -49,22 +59,22 @@ void	two_bit_shift_right(unsigned char key_56[], t_args *params)
 	finish_key_shift(key_res, params);
 }
 
-void set_two_left_bits(unsigned char key_res[], unsigned char key_56[],
+void	set_two_left_bits(unsigned char key_res[], unsigned char key_56[],
 int bit0, int bit1)
 {
 	bit0 = (1 << 3) & key_56[3];
 	bit1 = (1 << 2) & key_56[3];
-  key_res[4] = ((key_56[4] & 255) << 2) + (key_56[5] >> 6);
-  key_res[5] = ((key_56[5] & 255) << 2) + (key_56[6] >> 6);
-  key_res[6] = ((key_56[6] & 255) << 2);
-  if (bit0)
-    key_res[6] |= (1 << 1);
-  else
-    key_res[6] &= ~(1 << 1);
-  if (bit1)
-    key_res[6] |= (1 << 0);
-  else
-    key_res[6] &= ~(1 << 0);
+	key_res[4] = ((key_56[4] & 255) << 2) + (key_56[5] >> 6);
+	key_res[5] = ((key_56[5] & 255) << 2) + (key_56[6] >> 6);
+	key_res[6] = ((key_56[6] & 255) << 2);
+	if (bit0)
+		key_res[6] |= (1 << 1);
+	else
+		key_res[6] &= ~(1 << 1);
+	if (bit1)
+		key_res[6] |= (1 << 0);
+	else
+		key_res[6] &= ~(1 << 0);
 }
 
 void	two_bit_shift_left(unsigned char key_56[], t_args *params)
@@ -80,20 +90,18 @@ void	two_bit_shift_left(unsigned char key_56[], t_args *params)
 	key_res[0] = ((key_56[0] & 255) << 2) + (key_56[1] >> 6);
 	key_res[1] = ((key_56[1] & 255) << 2) + (key_56[2] >> 6);
 	key_res[2] = ((key_56[2] & 255) << 2) + (key_56[3] >> 6);
-  key_res[3] = ((key_56[3] & 255) << 2) + (key_56[4] >> 6);
-  if (bit0)
-    key_res[3] |= (1 << 5);
-  else
-    key_res[3] &= ~(1 << 5);
-  if (bit1)
-    key_res[3] |= (1 << 4);
-  else
-    key_res[3] &= ~(1 << 4);
+	key_res[3] = ((key_56[3] & 255) << 2) + (key_56[4] >> 6);
+	key_res[3] &= ~(1 << 5);
+	if (bit0)
+		key_res[3] |= (1 << 5);
+	key_res[3] &= ~(1 << 4);
+	if (bit1)
+		key_res[3] |= (1 << 4);
 	set_two_left_bits(key_res, key_56, bit0, bit1);
-	 while (i < 7)
-	 {
-	   (*params).key_res56[i] = key_res[i];
-	   i++;
-	 }
-  finish_key_shift(key_res, params);
+	while (i < 7)
+	{
+		(*params).key_res56[i] = key_res[i];
+		i++;
+	}
+	finish_key_shift(key_res, params);
 }
--- s_blocks_proc.c
+++ s_blocks_proc.c
@@ -1,34 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   s_blocks_proc.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vlikhotk <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/10 17:03:54 by vlikhotk          #+#    #+#             */
+/*   Updated: 2018/06/10 17:05:39 by vlikhotk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_ssl.h"
 
-//8 s-blocks
-int s1_block(int str, int col)
+int		s1_block(int str, int col)
 {
-	const int s_1[4][16] = {{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
-		{0, 15, 7, 4,	14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
-		{4, 1, 14, 8, 13, 6, 2, 11, 15, 12,	9, 7, 3, 10, 5, 0},
-		{15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}};
+	const int s_1[4][16] = {{14, 4, 13, 1, 2, 15, 11, 8, 3,
+	10, 6, 12, 5, 9, 0, 7}, {0, 15, 7, 4, 14, 2, 13, 1, 10,
+	6, 12, 11, 9, 5, 3, 8}, {4, 1, 14, 8, 13, 6, 2, 11, 15,
+	12, 9, 7, 3, 10, 5, 0}, {15, 12, 8, 2, 4, 9, 1, 7, 5,
+	11, 3, 14, 10, 0, 6, 13}};
+
 	return (s_1[str][col]);
 }
 
-int s2_block(int str, int col)
+int		s2_block(int str, int col)
 {
-	const int s_2[4][16] = {{15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
-	{3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
-	{0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
-	{13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}};
+	const int s_2[4][16] = {{15, 1, 8, 14, 6, 11, 3, 4, 9,
+	7, 2, 13, 12, 0, 5, 10}, {3, 13, 4, 7, 15, 2, 8, 14, 12,
+	0, 1, 10, 6, 9, 11, 5}, {0, 14, 7, 11, 10, 4, 13, 1, 5,
+	8, 12, 6, 9, 3, 2, 15}, {13, 8, 10, 1, 3, 15, 4, 2, 11,
+	6, 7, 12, 0, 5, 14, 9}};
+
 	return (s_2[str][col]);
 }
 
-int s3_block(int str, int col)
+int		s3_block(int str, int col)
 {
-	const int s_3[4][16] = {{10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
-	{13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
-	{13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
-	{1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}};
+	const int s_3[4][16] = {{10, 0, 9, 14, 6, 3, 15, 5, 1,
+	13, 12, 7, 11, 4, 2, 8}, {13, 7, 0, 9, 3, 4, 6, 10, 2,
+	8, 5, 14, 12, 11, 15, 1}, {13, 6, 4, 9, 8, 15, 3, 0,
+	11, 1, 2, 12, 5, 10, 14, 7}, {1, 10, 13, 0, 6, 9, 8,
+	7, 4, 15, 14, 3, 11, 5, 2, 12}};
+
 	return (s_3[str][col]);
 }
 
-void repeat_s_blocks_proccessing(t_des_enc *des_params, int *s_output, t_addition *iters, int **four_bits)
+void	repeat_s_blocks_proccessing(t_des_enc *des_params,
+int *s_output, t_addition *iters, int **four_bits)
 {
 	if ((*iters).i == 1)
 		*s_output = s2_block((*des_params).str_col[(*iters).i][0],
@@ -57,12 +75,15 @@ void repeat_s_blocks_proccessing(t_des_enc *des_params, int *s_output, t_additio
 	}
 }
 
-//Step 6.2 Receive string and column numbers
-void s_blocks_coords(t_des_enc *des_params, unsigned char exp_for_s[])
+/*
+** Step 6.2 Receive string and column numbers
+*/
+
+void	s_blocks_coords(t_des_enc *des_params, unsigned char exp_for_s[])
 {
 	int j;
 	int string[2];
- 	int column[4];
+	int column[4];
 
 	j = 0;
 	while (j < 8)
